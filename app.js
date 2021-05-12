d7e5c5f0f5ad0187ad98de8ea0eb47c08be1f29a
Igna Anezin <anezinignacio@gmail.com>
2021-05-12 15:54:33 -0300

    arreglos

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const pokedex = document.getElementById("pokedex");
-const searchField = document.getElementById("searchField");
-let pokeman = [];
+const searchField = document.getElementById("searchBar");
+let pokemon = [];
 
 searchField.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
   const searchString = e.target.value;
-  const filteredPokemon = pokeman.filter((pokeman) => {
-    return pokeman.name.includes(searchString) || pokeman.id == searchString;
+  const filteredPokemon = pokemon.filter((pokemon) => {
+    return pokemon.name.includes(searchString) || pokemon.id == searchString;
   });
   displayPokemon(filteredPokemon);
 });
 
 const fetchPokemon = () => {
   const promises = [];
-  for (let i = 1; i < 150; i++) {
+  for (let i = 1; i < 200; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((response) => response.json()));
   }
 
   Promise.all(promises).then((results) => {
-    const pokemon = results.map((data) => ({
+    pokemon = results.map((data) => ({
       name: data.name,
       image: data.sprites.other["dream_world"]["front_default"],
       types: data.types.map((type) => type.type.name).join(", "),
@@ -42,7 +43,7 @@ const displayPokemon = (pokemon) => {
   <li class="card">
     <h2 class="card-name">${pokeman.id}. ${pokeman.name}</h2>
     <img class="card-image" src="${pokeman.image}">
-    <p class="card-type">Type: ${pokeman.types}</p>
+    <p class="card-type">Tipo: ${pokeman.types}</p>
     <p class="card-xp">XP: ${pokeman.xp}</p>
     <p class="card-height">Altura: ${pokeman.height}</p>
     <p class="card-weight">Peso: ${pokeman.weight}</p>
